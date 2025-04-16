5c0ce0dc2fc5028b1eda8ed0d3510043783a963d
PC <nroztrk17@gmail.com>
2025-04-16 22:53:55 +0300

    TC_409 uptaded.

--- src/OpenMRS/methods/TC_409_Methods.java
+++ src/OpenMRS/methods/TC_409_Methods.java
@@ -1,6 +1,7 @@
 package OpenMRS.methods;
 
 import OpenMRS.elements.TC_409_Elements;
+import Utility.MyFunc;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -21,71 +22,12 @@ public class TC_409_Methods {
     }
 
     public void mergePatient() {
+        TC_404_Methods addPatient = new TC_404_Methods(driver, action, wait);
         elements = new TC_409_Elements(driver);
-        String searchByName = "Test User";
-        String givenName = "Test";
-        String familyName = "User";
-        String birthdayDay = "1";
-        String birthdayYear = "1990";
-        String address = "Test address";
-        String phoneNumber = "1111111111";
-        String personName = "Test";
+        String searchByName = "Benjamin Thomas Miller";
 
         for (int i = 0; i < 2; i++) {
-            wait.until(ExpectedConditions.elementToBeClickable(elements.registerBtn));
-            action.moveToElement(elements.registerBtn).click().build().perform();
-
-            wait.until(ExpectedConditions.visibilityOf(elements.givenName));
-            action.moveToElement(elements.givenName).sendKeys(givenName).build().perform();
-
-            wait.until(ExpectedConditions.visibilityOf(elements.familyName));
-            action.moveToElement(elements.familyName).click().sendKeys(familyName).build().perform();
-
-            wait.until(ExpectedConditions.elementToBeClickable(elements.nextBtn));
-            action.moveToElement(elements.nextBtn).click().build().perform();
-
-            Select gender = new Select(elements.gender);
-            gender.selectByIndex(1);
-
-            wait.until(ExpectedConditions.elementToBeClickable(elements.nextBtn));
-            action.moveToElement(elements.nextBtn).click().build().perform();
-
-            wait.until(ExpectedConditions.elementToBeClickable(elements.birthdayDay));
-            elements.birthdayDay.click();
-            action.sendKeys(birthdayDay).build().perform();
-
-            Select month = new Select(elements.birthdayMonth);
-            month.selectByValue("1");
-
-            wait.until(ExpectedConditions.visibilityOf(elements.birthdayYear));
-            action.moveToElement(elements.birthdayYear).click().sendKeys(birthdayYear).build().perform();
-
-            wait.until(ExpectedConditions.elementToBeClickable(elements.nextBtn));
-            action.moveToElement(elements.nextBtn).click().build().perform();
-
-            wait.until(ExpectedConditions.elementToBeClickable(elements.address));
-            action.moveToElement(elements.address).click().sendKeys(address).build().perform();
-
-            wait.until(ExpectedConditions.elementToBeClickable(elements.nextBtn));
-            action.moveToElement(elements.nextBtn).click().build().perform();
-
-            wait.until(ExpectedConditions.elementToBeClickable(elements.phoneNumber));
-            action.moveToElement(elements.phoneNumber).sendKeys(phoneNumber).build().perform();
-
-            wait.until(ExpectedConditions.elementToBeClickable(elements.nextBtn));
-            action.moveToElement(elements.nextBtn).click().build().perform();
-
-            Select relationshipType = new Select(elements.relationshipType);
-            relationshipType.selectByIndex(3);
-
-            wait.until(ExpectedConditions.visibilityOf(elements.personName));
-            action.scrollToElement(elements.personName).sendKeys(personName).build().perform();
-
-            wait.until(ExpectedConditions.elementToBeClickable(elements.nextBtn));
-            action.moveToElement(elements.nextBtn).click().build().perform();
-
-            wait.until(ExpectedConditions.elementToBeClickable(elements.confirmBtn));
-            action.moveToElement(elements.confirmBtn).click().build().perform();
+            addPatient.patientRegistrations();
 
             wait.until(ExpectedConditions.visibilityOf(elements.homeBtn));
             wait.until(ExpectedConditions.elementToBeClickable(elements.homeBtn)).click();
